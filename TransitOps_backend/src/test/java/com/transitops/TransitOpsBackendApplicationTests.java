cc709455d0ea20f6928a9fa5da495793a17145c3
sanjain <nigun.sanjai@autodesk.com>
2026-07-12 09:59:24 +0530

    feat: initial project structure with Spring Boot Gradle backend and Vite React TS frontend

---
+++ TransitOps_backend/src/test/java/com/transitops/TransitOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transitops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
